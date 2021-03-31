c19702eda18bf15ac721bd85e20c989fd53c1af3
thoanv <vanthoa225@gmail.com>
2021-03-31 17:22:47 +0700

    nt

--- lib/screens/home/components/popular_product.dart
+++ lib/screens/home/components/popular_product.dart
@@ -6,67 +6,35 @@ import '../../../size_config.dart';
 import 'section_title.dart';
 
 class PopularProducts extends StatelessWidget {
-  var p1 = ["A0", "A1", "A2", "A3"];
-  List<Widget> widgetlist = List();
-
-  MapPageState() {
-    getWidgets(widgetlist);
-  }
-
   @override
   Widget build(BuildContext context) {
-    double scwidth = MediaQuery.of(context).size.width;
-    double scheight = MediaQuery.of(context).size.height;
     return Column(
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        Container(
-          height: scheight - kToolbarHeight - 24,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(
-                  'https://images.wallpaperscraft.com/image/parking_cars_underground_131454_240x320.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: ListView(
-            children: widgetlist.map((element) {
-              return element;
-            }).toList(),
+      children: [
+        Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: SectionTitle(title: "Sản phẩm", press: () {}),
+        ),
+        SizedBox(height: getProportionateScreenWidth(20)),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Column(
+            children: [
+              ...List.generate(
+                demoProducts.length,
+                (index) {
+                  if (demoProducts[index].isPopular)
+                    return ProductCard(product: demoProducts[index]);
+
+                  return SizedBox
+                      .shrink(); // here by default width and height is 0
+                },
+              ),
+              SizedBox(width: getProportionateScreenWidth(20)),
+            ],
           ),
         )
       ],
     );
-  }
-  void getWidgets(List<Widget> wlist) {
-    for (int i = 0; i < p1.length-1; i++) {
-      wlist.add(Row(
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.green, borderRadius: BorderRadius.circular(20.0)),
-            child: FlatButton(
-              child: Text(
-                p1[i].toString(),
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () => print('hii1'),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.green, borderRadius: BorderRadius.circular(20.0)),
-            child: FlatButton(
-              child: Text(
-                p1[i + 1].toString(),
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () => print('hii2'),
-            ),
-          )
-        ],
-      ));
-    }
   }
 }

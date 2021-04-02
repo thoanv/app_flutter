3518cd581c39829b73c133ad4187b5e7dbc51321
thoanv <vanthoa225@gmail.com>
2021-04-02 14:26:54 +0700

    Thừa thư viên product_cart

--- lib/screens/filter/components/popular_product.dart
+++ lib/screens/filter/components/popular_product.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/components/product_card.dart';
 import 'package:shop_app/components/product_cart.dart';
 import 'package:shop_app/models/Product.dart';
 import 'package:shop_app/constants.dart';
